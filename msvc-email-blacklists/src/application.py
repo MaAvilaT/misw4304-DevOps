d15be1c99ab8d621a869233ad87049194b8052b3
TIBAQUIRA <tibaquirajoan@outlook.com>
2024-11-02 10:59:09 -0500

    Change to application

--- msvc-email-blacklists/src/application.py
+++ msvc-email-blacklists/src/application.py
@@ -4,12 +4,12 @@ from src.blueprints.msvc_email_blacklists import email_blacklists_blueprint
 from src.blueprints.msvc_management import management_blueprint
 from src.database.declarative_base import Base, engine
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.register_blueprint(management_blueprint)
-app.register_blueprint(email_blacklists_blueprint)
+application.register_blueprint(management_blueprint)
+application.register_blueprint(email_blacklists_blueprint)
 
 Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    application.run(host='0.0.0.0', port=5000, debug=True)
