25713351b663622c6f98925487e82ab38810b305
Edwin Silva <ea.silval1@uniandes.edu.co>
2024-11-30 18:41:14 -0500

    Change DB configuration in order to deploy RDS and ECS

--- msvc_email_blacklists/src/database/declarative_base.py
+++ msvc_email_blacklists/src/database/declarative_base.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_user = os.environ.get('RDS_USERNAME')
-db_password = os.environ.get('RDS_PASSWORD')
-db_host = os.environ.get('RDS_HOSTNAME')
-db_port = os.environ.get('RDS_PORT')
-db_name = os.environ.get('RDS_DB_NAME')
+db_user = os.environ.get('DATABASE_USER')
+db_password = os.environ.get('DATABASE_PASSWORD')
+db_host = os.environ.get('DATABASE_HOST')
+db_port = os.environ.get('DATABASE_PORT')
+db_name = os.environ.get('DATABASE_NAME')
 
 # DB_PATH = os.environ.get('DATABASE_URL')
 
